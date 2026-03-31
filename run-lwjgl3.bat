c182c04aff3ea210776ec547f1dbc23f91d35f36
yusufgonen <yusuf.gonen@ug.bilkent.edu.tr>
2026-03-31 14:15:37 +0300

    Tried something

--- run-lwjgl3.bat
+++ run-lwjgl3.bat
@@ -1,5 +1,9 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-call .\gradlew.bat :lwjgl3:run
+call .\gradlew.bat --stop
+taskkill /F /IM java.exe >nul 2>&1
+taskkill /F /IM javaw.exe >nul 2>&1
+powershell -NoProfile -Command "Remove-Item -Recurse -Force \"$env:USERPROFILE\\.gradle\\caches\\modules-2\\files-2.1\\org.lwjgl\\lwjgl\" -ErrorAction SilentlyContinue"
+call .\gradlew.bat --refresh-dependencies :lwjgl3:run
 endlocal
